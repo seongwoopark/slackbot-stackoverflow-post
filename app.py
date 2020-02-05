cd3bc3bfe8f6bed02a55dbc1fbde4b2830c78a3e
Seongwoo Park <seongwoo@mymusictaste.com>
2020-02-05 20:17:24 +0900

    add socketio flask

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 import os
 
 import slack
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+socketio = SocketIO(app)
 
 
-# @slack.RTMClient.run_on(event='message')
-# def say_hello(**payload):
-#     data = payload['data']
-#     web_client = payload['web_client']
-#     rtm_client = payload['rtm_client']
-#     if 'Hello' in data.get('text', []):
-#         channel_id = data['channel']
-#         thread_ts = data['ts']
-#         user = data['user']
-#
-#         web_client.chat_postMessage(
-#             channel=channel_id,
-#             text=f"Hi <@{user}>!",
-#             thread_ts=thread_ts
-#         )
-#
-#
-# def index(request):
-#     slack_token = os.environ["SLACK_API_TOKEN"]
-#     rtm_client = slack.RTMClient(token=slack_token)
-#     rtm_client.start()
-#     return HttpResponse('Done')
+@slack.RTMClient.run_on(event='message')
+def say_hello(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    if 'Hello' in data.get('text', []):
+        channel_id = data['channel']
+        thread_ts = data['ts']
+        user = data['user']
+
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Hi <@{user}>!",
+            thread_ts=thread_ts
+        )
 
 
 @app.route('/')
@@ -36,5 +32,12 @@ def index():
     return "<h1>Welcome to our server !!</h1>"
 
 
+@socketio.on('/bot')
+def work(message):
+    slack_token = os.environ["SLACK_API_TOKEN"]
+    rtm_client = slack.RTMClient(token=slack_token)
+    rtm_client.start()
+
+
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    socketio.run(app=app)
